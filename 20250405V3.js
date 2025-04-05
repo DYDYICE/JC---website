1dfdb91a07b1d61cf8e3ff0af14aa9a81e23c06c
DYDYICE <info@ice-mountain.com>
2025-04-05 14:15:17 +0200

    Create 20250405V3.js

---
+++ 20250405V3.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Kolomtitels
+  document.querySelector(".col-dates").innerHTML = "🎟️ " + document.querySelector(".col-dates").innerHTML;
+  document.querySelector(".col-flex").innerHTML = "🎟️ " + document.querySelector(".col-flex").innerHTML;
+
+  // Tarieflijnen
+  document.querySelector(".row-90").innerHTML = "👶 " + document.querySelector(".row-90").innerHTML;
+  document.querySelector(".row-child").innerHTML = "🙂 " + document.querySelector(".row-child").innerHTML;
+  document.querySelector(".row-adult").innerHTML = "🧍 " + document.querySelector(".row-adult").innerHTML;
+  document.querySelector(".row-anniv").innerHTML = "🎉 " + document.querySelector(".row-anniv").innerHTML;
+});
