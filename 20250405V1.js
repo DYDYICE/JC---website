86187466559b9999749129ee57ef037f9d78fafb
DYDYICE <info@ice-mountain.com>
2025-04-05 14:07:58 +0200

    Create 20250405V1.js

---
+++ 20250405V1.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".datés").innerHTML = "🎟️ " + document.querySelector(".datés").innerHTML;
+  document.querySelector(".flex").innerHTML = "🎟️ " + document.querySelector(".flex").innerHTML;
+  document.querySelector(".child").innerHTML = "🧒 " + document.querySelector(".child").innerHTML;
+  document.querySelector(".adult").innerHTML = "👨 " + document.querySelector(".adult").innerHTML;
+  document.querySelector(".goolfy").innerHTML = "🎯 " + document.querySelector(".goolfy").innerHTML;
+  document.querySelector(".combi").innerHTML = "🌀 " + document.querySelector(".combi").innerHTML;
+  document.querySelector(".abo-child").innerHTML = "🧾 " + document.querySelector(".abo-child").innerHTML;
+  document.querySelector(".abo-adult").innerHTML = "🧾 " + document.querySelector(".abo-adult").innerHTML;
+  document.querySelector(".anniv").innerHTML = "🎉 " + document.querySelector(".anniv").innerHTML;
+});
